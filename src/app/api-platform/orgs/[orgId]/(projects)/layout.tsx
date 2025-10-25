ab48c6270aa6eea6883eea1b753389c27158e08f
Botir Khaltaev <btrghstk@gmail.com>
2025-10-25 22:38:11 +0100

    refactor: remove UnsavedFormsProvider and simplify ProjectsLayout component to streamline form management

--- src/app/api-platform/orgs/[orgId]/(projects)/layout.tsx
+++ src/app/api-platform/orgs/[orgId]/(projects)/layout.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { UnsavedFormsProvider } from "@/context/unsaved-forms-context";
-
 export default function ProjectsLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return <UnsavedFormsProvider>{children}</UnsavedFormsProvider>;
+	return <>{children}</>;
 }
